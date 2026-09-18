267d77dbe4b0c598c3cd57fb6ca1de5999ee66f1
Othanskie <maharlikaeast01@gmail.com>
2026-09-18 16:37:11 +0800

    Add files via upload

---
+++ DRUMLAB-iPad/AppDelegate.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+@main
+final class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions options: [UIApplication.LaunchOptionsKey: Any]? = nil) -> Bool {
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = ViewController()
+        window?.makeKeyAndVisible()
+        return true
+    }
+}
